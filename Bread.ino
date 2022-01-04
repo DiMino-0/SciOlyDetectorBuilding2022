c0088977b813ad0c8997b7e26b68f2d7b5a5b06d
Joseph DiMino <josephdimino05@gmail.com>
2022-01-04 16:08:46 -0500

    commit

--- Bread.ino
+++ Bread.ino
@@ -1,16 +1,15 @@
-//3 pins, one blue, one red, one yellow
+//Bounds are in PPM NaCI
+int BOUND_1 = 1000;
+int BOUND_2 = 2000;
+int BOUND_3 = 3000;
+
 int YELLOW_LED = 1;
 int RED_LED = 2;
 int BLUE_LED = 3;
-int sensor = A0;
 
-//LEDS
-int voltage = 0;
-int bound1 = 0;
-int bound2 = 0;
-int bound3 = 0;
-
-//constants for equation?
+int SENSOR = A0;
+double input = 0;
+double voltage = 0;
 int salinity = 0;
 
 void setup() 
@@ -19,50 +18,50 @@ void setup()
     pinMode(RED_LED, OUTPUT);
     pinMode(BLUE_LED, OUTPUT);
 
-    pinMode(sensor, INPUT);
+    pinMode(SENSOR, INPUT);
 
     Serial.begin(9600);
 }
 void loop() 
 {
-    voltage = analogRead(sensor);
-    digitalWrite(RED_LED, HIGH);
-    digitalWrite(YELLOW_LED, HIGH);
-    digitalWrite(BLUE_LED, HIGH);
+    //4.9 m/v per unit
+    input = analogRead(SENSOR);
 
-    //get salinity using resitance
+    //ADC equation
+    voltage = input * (5.0/1023.0);
+
+    //Voltage to PPM equation
     salinity = (voltage * 0.01);
 
-    if (salinity <= 0.1)
-    {
-        digitalWrite(RED_LED, LOW);
-        digitalWrite(YELLOW_LED, LOW);
-        digitalWrite(BLUE_LED, HIGH);
-    }
-    else if (salinity > 0.1 && salinity < 0.2)
-    {
-        digitalWrite(RED_LED, LOW);
-        digitalWrite(YELLOW_LED, HIGH);
-        digitalWrite(BLUE_LED, LOW);
-    }
-    else if (salinity >= 0.2 && salinity <= 0.3)
+    //BOUNDS to highlight the LEDs
+    if (salinity <= BOUND_1)
     {
         digitalWrite(RED_LED, HIGH);
         digitalWrite(YELLOW_LED, LOW);
         digitalWrite(BLUE_LED, LOW);
     }
-    else if (salinity > 0.3)
+    else if (salinity > BOUND_1 && salinity <= BOUND_2)
     {
-        digitalWrite(RED_LED, HIGH);
+        digitalWrite(RED_LED, LOW);
         digitalWrite(YELLOW_LED, HIGH);
+        digitalWrite(BLUE_LED, LOW);
+    }
+    else if (salinity > BOUND_2 && salinity <= BOUND_3)
+    {
+        digitalWrite(RED_LED, LOW);
+        digitalWrite(YELLOW_LED, LOW);
         digitalWrite(BLUE_LED, HIGH);
     }
 
+    //output to the serial monitor
     Serial.print("Voltage: ");
     Serial.print(voltage);
-    Serial.println(" Ohms");
+    Serial.print(" V");
+    Serial.print(" and ");
     Serial.print("Salinity: ");
     Serial.print(salinity);
     Serial.println(" ppm");
-    delay(1000);
+
+    //In milliseconds
+    delay(8000);
 }
